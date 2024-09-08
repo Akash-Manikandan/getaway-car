857dc8a1e1209cc038110d76cac286348de71535
Aishwarya <92903869+AishwaryaSubash@users.noreply.github.com>
2024-09-08 20:11:26 +0530

    services done (#5)

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
+import { DrizzleModule } from 'src/drizzle/drizzle.module';
 
 @Module({
+  imports: [DrizzleModule],
   controllers: [AccountsController],
   providers: [AccountsService],
 })
--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { CreateAccountDto } from './dto/create-account.dto';
-import { UpdateAccountDto } from './dto/update-account.dto';
+// import { UpdateAccountDto } from './dto/update-account.dto';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { DrizzleAsyncProvider } from 'src/drizzle/drizzle.provider';
 import * as schema from '../drizzle/schema';
@@ -13,16 +13,19 @@ export class AccountsService {
     private db: NodePgDatabase<typeof schema>,
   ) {}
 
-  async create(createAccountDto: CreateAccountDto) {
-    return `This action returns all accounts`;
+  async createAccount(createAccountDto: CreateAccountDto) {
+    await this.db.insert(schema.accounts).values({
+      name: createAccountDto.name,
+      country: createAccountDto.country,
+    });
   }
 
-  async findAll() {
+  async findAllAccounts() {
     const result = await this.db.select().from(schema.accounts);
     return result;
   }
 
-  async findOne(id: string) {
+  async findOneAccount(id: string) {
     const result = await this.db
       .select()
       .from(schema.accounts)
@@ -30,11 +33,14 @@ export class AccountsService {
     return result;
   }
 
-  update(id: number, updateAccountDto: UpdateAccountDto) {
-    return `This action updates a #${id} account`;
+  async deactivateAccount(id: string) {
+    await this.db
+      .update(schema.accounts)
+      .set({ name: 'hi' })
+      .where(eq(schema.accounts.id, id));
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} account`;
-  }
+  async editName() {}
+
+  async editCountry() {}
 }
