f54532c8d594ef2b4454993ddc147652f961b7b2
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2024-09-08 20:24:50 +0530

    Dev tools (#6)

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,23 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Identity provider')
+    .setDescription('The Identity provider API description')
+    .setVersion('1.0')
+    .addTag('Identity provider')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
