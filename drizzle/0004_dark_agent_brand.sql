3de8f93cccce0d2f888c517fa77ebdffa2a525ad
AishwaryaSubash <riya129ash@gmail.com>
2024-09-08 19:39:52 +0530

    db migration

---
+++ drizzle/0004_dark_agent_brand.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "accounts" RENAME COLUMN "region" TO "country";
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { DrizzleModule } from './drizzle/drizzle.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { AccountsModule } from './accounts/accounts.module';
 
 @Module({
   imports: [
     DrizzleModule,
     ConfigModule.forRoot({ isGlobal: true }),
     UsersModule,
+    AccountsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
