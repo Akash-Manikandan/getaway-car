dcd80769d13e0b83fd16e269ac00af6993182fab
Aishwarya <92903869+AishwaryaSubash@users.noreply.github.com>
2024-09-21 21:06:54 +0530

    feat - PROJ-123 - Add data modeling (#11)

--- drizzle/0002_goofy_warlock.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "accounts" ADD COLUMN "api_key" varchar DEFAULT gen_random_uuid() NOT NULL;--> statement-breakpoint
-ALTER TABLE "permissions" ADD COLUMN "extras" jsonb DEFAULT '{}'::jsonb;--> statement-breakpoint
-ALTER TABLE "roles" ADD COLUMN "extras" jsonb DEFAULT '{}'::jsonb;--> statement-breakpoint
-ALTER TABLE "services" ADD COLUMN "extras" jsonb DEFAULT '{}'::jsonb;--> statement-breakpoint
-ALTER TABLE "accounts" ADD CONSTRAINT "accounts_api_key_unique" UNIQUE("api_key");
